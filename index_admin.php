8f6efd6ff0bff34b72f4a5b5933764ea303e3ec2
PrasadHendry <el.verethragna@gmail.com>
2022-01-10 11:10:52 +0530

    msg'

--- index_admin.php
+++ index_admin.php
@@ -109,7 +109,7 @@
     </div>
     <!-- /.content-wrapper -->
     <footer class="main-footer">
-      <strong>Copyright &copy; 2021-2022 <a href="#">EVA</a>.</strong>
+      <strong>Copyright &copy; 2021-2022 <a href="https://www.instagram.com/evangelion.official/?hl=en">EVA</a>.</strong>
       All rights reserved.
       <div class="float-right d-none d-sm-inline-block">
         <b>Version</b> 0.1.1
