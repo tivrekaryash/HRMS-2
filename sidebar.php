d2a8d0430714d8a65742f2843e0ad1f05c16949d
PrasadHendry <el.verethragna@gmail.com>
2022-01-10 13:32:12 +0530

    msg

--- sidebar.php
+++ sidebar.php
@@ -1,87 +1,86 @@
 <!-- Brand Logo -->
 <a href="index_admin.php" class="brand-link">
-    <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-    <span class="brand-text font-weight-light"> HRMS </span>
+  <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+  <span class="brand-text font-weight-light"> HRMS </span>
 </a>
 <div class="sidebar">
-    <!-- Sidebar user panel (optional) -->
-    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-            <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-            <a href="#" class="d-block">Admin - Test</a>
-        </div>
+  <!-- Sidebar user panel (optional) -->
+  <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+    <div class="image">
+      <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
     </div>
-
-    <!-- SidebarSearch Form -->
-    <div class="form-inline">
-        <div class="input-group" data-widget="sidebar-search">
-            <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-            <div class="input-group-append">
-                <button class="btn btn-sidebar">
-                    <i class="fas fa-search fa-fw"></i>
-                </button>
-            </div>
-        </div>
+    <div class="info">
+      <a href="#" class="d-block">Admin - Test</a>
     </div>
+  </div>
 
-    <!-- Sidebar Menu -->
-    <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-            <!-- Add icons to the links using the .nav-icon class
+  <!-- SidebarSearch Form -->
+  <div class="form-inline">
+    <div class="input-group" data-widget="sidebar-search">
+      <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+      <div class="input-group-append">
+        <button class="btn btn-sidebar">
+          <i class="fas fa-search fa-fw"></i>
+        </button>
+      </div>
+    </div>
+  </div>
+
+  <!-- Sidebar Menu -->
+  <nav class="mt-2">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+      <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-            <li class="nav-item menu-open">
-                <a href="index_admin.php" class="nav-link">
-                    <i class="nav-icon fas fa-tachometer-alt"></i>
-                    <p>
-                        Dashboard
-                    </p>
-                </a>
-            </li>
-            <li class="nav-item">
-            <a href="candidate_details.php" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
-              <p>
-                Candidate Details
-                <span class="badge badge-danger right">0</span>
-              </p>
-            </a>
-          </li>
+      <li class="nav-item menu-open">
+        <a href="index_admin.php" class="nav-link">
+          <i class="nav-icon fas fa-tachometer-alt"></i>
+          <p>
+            Dashboard
+          </p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="candidate_details.php" class="nav-link">
+          <i class="nav-icon fas fa-th"></i>
+          <p>
+            Candidate Details
+            <span class="badge badge-danger right">0</span>
+          </p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="emp_details.php" class="nav-link">
+          <i class="nav-icon fas fa-th"></i>
+          <p>
+            Employee Details
+            <span class="badge badge-info right">0</span>
+          </p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link">
+          <i class="nav-icon far fa-building"></i>
+          <p>
+            Company Structure
+            <i class="right fas fa-angle-left"></i>
+          </p>
+        </a>
+        <ul class="nav nav-treeview">
           <li class="nav-item">
-            <a href="emp_details.php" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
-              <p>
-                Employee Details
-                <span class="badge badge-info right">0</span>
-              </p>
+            <a href="#" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Departments</p>
             </a>
           </li>
           <li class="nav-item">
             <a href="#" class="nav-link">
-              <i class="nav-icon far fa-building"></i>
-              <p>
-                Company Structure
-                <i class="right fas fa-angle-left"></i>
-              </p>
+              <i class="far fa-circle nav-icon"></i>
+              <p>Designations</p>
             </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Departments</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Designations</p>
-                </a>
-              </li>
-            </ul>
           </li>
-
         </ul>
-    </nav>
-    <!-- /.sidebar-menu -->
+      </li>
+    </ul>
+  </nav>
+  <!-- /.sidebar-menu -->
 </div>
