ace3f1dd0731372bc90117ba083aa0fcba9b62fc
PrasadHendry <el.verethragna@gmail.com>
2022-01-11 12:55:43 +0530

    msg

--- emp_upd_query.php
+++ emp_upd_query.php
@@ -20,6 +20,6 @@ $res = $conn->query("UPDATE employee_phnum SET phnum_home='$phnum_hm', phnum_wor
 
 // redirects to display employee information after closing connection
 $conn->close();
-header("location:employee_details.php");
+header("location:emp_details.php");
 exit;
 ?>
