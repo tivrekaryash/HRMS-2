131ab464247711ba712922bddd6ac9f53e81daad
SidWasTaken <siddharth.shyampur@gmail.com>
2022-01-11 12:46:22 +0530

    msg

--- emp_upd.php
+++ emp_upd.php
@@ -1,10 +1,39 @@
 <?php
 // db connection file
-include 'db_conn.php';
+require_once 'db_conn.php';
 
 $res = $conn->query("select * from employee_phnum where employee_id = $row[employee_id]");
 $res = $res->fetch_assoc();
 ?>
+
+<script>
+    // function to perform validations
+    function checkForm() {
+        var fname = document.forms["emp_upd"]["fname_upd"].value;
+        var age = document.forms["emp_upd"]["age_upd"].value;
+        var email = document.forms["emp_upd"]["email_upd"].value;
+
+        // checking for digits in full name
+        if (!(/^[A-Za-z\s]{1,}[\.]{0,1}[A-Za-z\s]{0,}$/.test(fname))) {
+            window.alert("Full name is invalid");
+            return false;
+        }
+
+        // checking for age, valid age is 18 to 60
+        if (!(/^\d{2}$/.test(age)) || age > 60 || age < 18) {
+            window.alert("age is invalid");
+            return false;
+        }
+
+        // checking validity of email
+        if (!(/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/.test(email))) {
+            window.alert("email is invalid");
+            return false;
+        }
+
+        return true;
+    }
+</script>
 
 <!-- Modal update -->
 <div class="modal fade" id="modal_update<?php echo $row["employee_id"]; ?>" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
@@ -19,78 +48,79 @@ $res = $res->fetch_assoc();
             <div class="modal-body">
                 <div class="container p-5 my-2 border">
                     <h2>Update your details here:</h2><br>
-                    <form name="emp_upd" action=".php" method="POST">
+                    <form name="emp_upd" onsubmit="return checkForm()" action="emp_upd_query.php" method="POST">
 
                         <div class="form-group">
-                            <label for="fullname" class="form-label">Full Name: </label>
-                            <input type="text" class="form-control" id="fname" name="fullname" value = "<?php echo $row["employee_name"]; ?>" required>
+                            <label for="fname_upd" class="form-label">Full Name: </label>
+                            <input type="text" class="form-control" id="fname_upd" name="fname_upd" value="<?php echo $row["employee_name"]; ?>" required>
                         </div>
                         <br>
 
                         <div class="form-inline">
-                            <label for="dob" class="form-label">Date of Birth: </label>
+                            <label for="dob_upd" class="form-label">Date of Birth: </label>
                             <div class="col-sm-2">
-                                <input type="date" class="form-control" id="dob" name="dateofbirth" value = "<?php echo $row["employee_dob"]; ?>" required>
+                                <input type="date" class="form-control" id="dob_upd" name="dob_upd" value="<?php echo $row["employee_dob"]; ?>" required>
                             </div>
                         </div>
                         <br>
 
                         <div class="form-inline">
-                            <label for="age" class="form-label">Age: </label>
+                            <label for="age_upd" class="form-label">Age: </label>
                             <div class="col-sm-2">
-                                <input type="number" class="form-control" id="age" name="age" value = "<?php echo $row["employee_age"]; ?>" required>
+                                <input type="number" class="form-control" id="age_upd" name="age_upd" value="<?php echo $row["employee_age"]; ?>" required>
                             </div>
                         </div>
                         <br>
 
                         <div class="form-check">
-                            <label for="gender" class="form-label">Gender: </label>
+                            <label for="gender_upd" class="form-label">Gender: </label>
                             <div class="form-check">
-                                <input type="radio" class="form-check-input" id="gender" name="gender" <?php if ($row["employee_gender"]=="Male") echo "checked";?> value="Male">Male
+                                <input type="radio" class="form-check-input" id="gender_upd" name="gender_upd" <?php if ($row["employee_gender"] == "Male") echo "checked"; ?> value="Male">Male
                                 <label class="form-check-label" for="radio1"></label>
                             </div>
                             <div class="form-check">
-                                <input type="radio" class="form-check-input" id="gender" name="gender" <?php if ($row["employee_gender"]=="Female") echo "checked";?> value="Female">Female
+                                <input type="radio" class="form-check-input" id="gender_upd" name="gender_upd" <?php if ($row["employee_gender"] == "Female") echo "checked"; ?> value="Female">Female
                                 <label class="form-check-label" for="radio1"></label>
                             </div>
                             <div class="form-check">
-                                <input type="radio" class="form-check-input" id="gender" name="gender" <?php if ($row["employee_gender"]=="Others") echo "checked";?> value="Others">Others
+                                <input type="radio" class="form-check-input" id="gender_upd" name="gender_upd" <?php if ($row["employee_gender"] == "Others") echo "checked"; ?> value="Others">Others
                                 <label class="form-check-label" for="radio1"></label>
                             </div>
                         </div>
                         <br>
 
                         <div class="form-group">
-                            <label for="address" class="form-label">Address: </label>
-                            <textarea type="text" class="form-control" rows="5" cols="33" id="address" name="address" required><?php echo $row["employee_address"]; ?></textarea>
+                            <label for="address_upd" class="form-label">Address: </label>
+                            <textarea type="text" class="form-control" rows="5" cols="33" id="address_upd" name="address_upd" required><?php echo $row["employee_address"]; ?></textarea>
                         </div>
                         <br>
 
                         <div class="form-group">
-                            <label for="email" class="form-label">Email address: </label>
-                            <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" value = "<?php echo $row["employee_email"]; ?>" required>
+                            <label for="email_upd" class="form-label">Email address: </label>
+                            <input type="email" class="form-control" id="email_upd" name="email_upd" aria-describedby="emailHelp" value="<?php echo $row["employee_email"]; ?>" required>
                             <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                         </div>
                         <br>
 
                         <div class="form-group">
                             <label for="phnum_hm" class="form-label">Phone (Home) number: (+91) </label>
-                            <input type="number_format" class="form-control" id="phnum_hm" name="phnum_hm" minlength="10" maxlength="10" value = "<?php echo $res["phnum_home"]; ?>" required>
+                            <input type="number_format" class="form-control" id="phnum_hm" name="phnum_hm" minlength="10" maxlength="10" value="<?php echo $res["phnum_home"]; ?>" required>
                         </div>
                         <br>
 
                         <div class="form-group">
                             <label for="phnum_wk" class="form-label">Phone (work) number: (+91) </label>
-                            <input type="number_format" class="form-control" id="phnum_wk" name="phnum_wk" minlength="10" maxlength="10" value = "<?php echo $res["phnum_work"]; ?>" required>
+                            <input type="number_format" class="form-control" id="phnum_wk" name="phnum_wk" minlength="10" maxlength="10" value="<?php echo $res["phnum_work"]; ?>" required>
                         </div>
                         <br>
 
                         <div class="form-group">
                             <label for="phnum_mb" class="form-label">Phone (Mobile) number: (+91) </label>
-                            <input type="number_format" class="form-control" id="phnum_mb" name="phnum_mb" minlength="10" maxlength="10" value = "<?php echo $res["phnum_mobile"]; ?>" required>
+                            <input type="number_format" class="form-control" id="phnum_mb" name="phnum_mb" minlength="10" maxlength="10" value="<?php echo $res["phnum_mobile"]; ?>" required>
                         </div>
                         <br>
 
+                        <input type="hidden" id="eid" name="eid" value="<?php echo $row["employee_id"]; ?>">
 
                         <button type="submit" class="btn btn-primary" style="float: right;">Submit</button>
 
---
+++ emp_upd_query.php
@@ -0,0 +1,25 @@
+<?php
+// db connection file
+require_once 'db_conn.php';
+
+// fetches updated data from the form
+$fullname = $_POST['fname_upd'];
+$dateofbirth = $_POST['dob_upd'];
+$age = $_POST['age_upd'];
+$gender = $_POST['gender_upd'];
+$address = $_POST['address_upd'];
+$email = $_POST['email_upd'];
+$phnum_hm = $_POST['phnum_hm'];
+$phnum_wk = $_POST['phnum_wk'];
+$phnum_mb = $_POST['phnum_mb'];
+$upd_id = $_POST['eid'];
+
+// stores updated data into the database
+$res = $conn->query("UPDATE employee_information SET employee_name='$fullname', employee_dob='$dateofbirth', employee_age='$age', employee_gender='$gender', employee_address='$address', employee_email='$email' WHERE employee_id='$upd_id'");
+$res = $conn->query("UPDATE employee_phnum SET phnum_home='$phnum_hm', phnum_work='$phnum_wk', phnum_mobile='$phnum_mb' WHERE employee_id='$upd_id'");
+
+// redirects to display employee information after closing connection
+$conn->close();
+header("location:employee_details.php");
+exit;
+?>
