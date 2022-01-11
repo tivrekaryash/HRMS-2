653452301c3224f21019a225a2e12007de237e76
SidWasTaken <siddharth.shyampur@gmail.com>
2022-01-11 10:25:17 +0530

    msg

--- candidate_upd_query.php
+++ candidate_upd_query.php
@@ -1,6 +1,6 @@
 <?php
 // db connection file
-include 'db_conn.php';
+require_once 'db_conn.php';
 
 // fetches updated data from the form
 $fullname = $_POST['fname_upd'];
@@ -15,8 +15,8 @@ $qualif = $_POST['qualif_upd'];
 $upd_id = $_POST['cid'];
 
 // stores updated data into the database
-$res = mysqli_query($conn, "UPDATE candidate_information SET candidate_fullname=$fullname, candidate_dob=$dateofbirth, candidate_age=$age, candidate_gender=$gender, candidate_address=$address, phnum=$phnumber, candidate_email=$email, experience_in_years=$workexperience WHERE candidate_id=$upd_id");
-$res = mysqli_query($conn, "UPDATE candidate_qualifications SET qualifications_file_location=$qualif WHERE candidate_id=$upd_id");
+$res = $conn->query("UPDATE candidate_information SET (candidate_fullname=$fullname, candidate_dob=$dateofbirth, candidate_age=$age, candidate_gender=$gender, candidate_address=$address, phnum=$phnumber, candidate_email=$email, experience_in_years=$workexperience) WHERE candidate_id=$upd_id");
+$res = $conn->query("UPDATE candidate_qualifications SET (qualifications_file_location=$qualif) WHERE candidate_id=$upd_id");
 
 // redirects to display candidate information after closing connection
 $conn->close();
