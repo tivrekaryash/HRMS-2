35a4ece3093e2eb430a4e345a09204cc279c3aea
PrasadHendry <el.verethragna@gmail.com>
2022-01-11 11:31:32 +0530

    msg

--- candidate_upd.php
+++ candidate_upd.php
@@ -128,9 +128,6 @@
                     </form>
                 </div>
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            </div>
         </div>
     </div>
 </div>
--- emp_upd.php
+++ emp_upd.php
@@ -97,9 +97,6 @@ $res = $res->fetch_assoc();
                     </form>
                 </div>
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            </div>
         </div>
     </div>
 </div>
