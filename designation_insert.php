d633baa8236512af22f85e03bc0c161c38830b0c
SidWasTaken <siddharth.shyampur@gmail.com>
2022-01-25 22:06:35 +0530

    msg

---
+++ designation_insert.php
@@ -0,0 +1,16 @@
+<?php
+// db connection file
+include 'db_conn.php';
+
+$department = $_POST['deptname'];
+$desig = $_POST['desgname'];
+$salary = $_POST['basesal'];
+
+// inserting department
+$res = mysqli_query($conn, "INSERT INTO designations (department_id, designation, base_salary) values ((select department_id from department where department_name = '$department'), '$desig', '$salary')");
+
+// redirects to display candidate information after closing connection
+$conn->close();
+header("location:cmp_struct_view.php");
+exit;
+?>
