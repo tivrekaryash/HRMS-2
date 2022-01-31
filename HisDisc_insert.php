9bf5a41af670b416bc7f8e77a3768931e5e4595a
SidWasTaken <siddharth.shyampur@gmail.com>
2022-01-31 21:42:55 +0530

    msg

--- HisDisc_insert.php
+++ HisDisc_insert.php
@@ -8,10 +8,10 @@ $beh_std = $_POST['best'];
 $dis_action = $_POST['disc'];
 
 // inserting job history
-$res = mysqli_query($conn, "INSERT INTO disciplinary_history (employee_id, behavior_standard, disciplinary_action, date) values ('$emp_id', '$beh_std', '$dis_action', '$date')");
+$res = mysqli_query($conn, "INSERT INTO disciplinary_history (employee_id, behaviour_standard, disciplinary_action, dis_date) values ('$emp_id', '$beh_std', '$dis_action', '$date')");
 
 // redirects to display candidate information after closing connection
-$conn->close();
-header("location:history_rec_view.php?c=1");
-exit;
+//$conn->close();
+//header("location:history_rec_view.php?c=1");
+//exit;
 ?>
