1d5a3517d73ecbfa67c4a3130ce8e59c555121cb
SidWasTaken <siddharth.shyampur@gmail.com>
2022-01-31 20:07:22 +0530

    msg

--- HisJob_insert.php
+++ HisJob_insert.php
@@ -17,6 +17,6 @@ $res = mysqli_query($conn, "INSERT INTO job_history (employee_id, designation_id
 
 // redirects to display candidate information after closing connection
 $conn->close();
-header("location:history_rec_view.php");
+header("location:history_rec_view.php?c=0");
 exit;
 ?>
