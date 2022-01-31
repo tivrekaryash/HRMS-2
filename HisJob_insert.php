5281d22a723c899e2cc3eb8c486b1e01aed0e958
SidWasTaken <siddharth.shyampur@gmail.com>
2022-01-31 21:14:52 +0530

    msg

--- HisJob_insert.php
+++ HisJob_insert.php
@@ -9,10 +9,10 @@ $desig_id = $_POST['dname'];
 $job_desc = $_POST['jobdesc'];
 
 // inserting job history
-$res = mysqli_query($conn, "INSERT INTO job_history (employee_id, designation_id, job_start_date, job_end_date, job_description) values ('$emp_id', '$desig_id, '$job_start', '$job_end', '$job_desc')");
+$res = mysqli_query($conn, "INSERT INTO job_history (employee_id, designation_id, job_start_date, job_end_date, job_description) values ('$emp_id', '$desig_id', '$job_start', '$job_end', '$job_desc')");
 
 // redirects to display candidate information after closing connection
-//$conn->close();
-//header("location:history_rec_view.php?c=0");
-//exit;
+$conn->close();
+header("location:history_rec_view.php?c=0");
+exit;
 ?>
