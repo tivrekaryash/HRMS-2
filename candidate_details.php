bbc683104a8d021e7f17ea8107868f11be6246fe
PrasadHendry <el.verethragna@gmail.com>
2022-02-03 18:03:35 +0530

    msg

--- candidate_details.php
+++ candidate_details.php
@@ -91,7 +91,7 @@ include 'db_conn.php';
                             } else {
                             ?>
                                 <td><button type="submit" class="btn btn-warning" data-toggle="modal" data-target="#modal_update<?php echo $row["candidate_id"]; ?>">Update</button></td>
-                                <td><button type='submit' class="btn btn-success" data-toggle="modal" data-target="#modal_accept<?php echo $row["candidate_id"]; ?>">Accept</button></td>
+                                <td><a href='emp_info.php?acc=<?php echo $row["candidate_id"]; ?>'><button type='submit' class='btn btn-success'>Accept</button></td>
                                 </tr>
                     <?php
                             }
@@ -217,56 +217,6 @@ include 'db_conn.php';
                                 <input type="text" class="form-control" id="qualif" name="qualifications" required>
                             </div>
                             <br>
-
-                            <button type="submit" class="btn btn-primary" style="float: right;">Submit</button>
-
-                        </form>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <br>
-                </div>
-            </div>
-        </div>
-    </div><!-- /.Modal -->
-
-    <!-- Modal-Acceptance -->
-    <div class="modal fade" id="smodal_accept<?php echo $row["candidate_id"]; ?>" data-backdrop="static" data-keyboard="false" aria-labelledby="staticBackdropLabel" aria-hidden="true" style="overflow:hidden;">
-        <div class="modal-dialog modal-dialog-scrollable modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Acceptance form: </h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div class="container p-5 my-2 border">
-                        <h2>Assign designation:</h2><br>
-                        <form name="accp_form" action="emp_info.php?acc=<?php echo $row["candidate_id"]; ?>" method="POST">
-
-                            <div class="form-group">
-                                <label for="fname" class="form-label">Full Name: </label>
-                                <input type="text" class="form-control" id="fname" name="fname" value="<?php echo $row["candidate_fullname"]; ?>" disabled>
-                            </div>
-                            <br>
-
-                            <div class="form-group">
-                                <label for="dname" class="form-label">Select Designation: </label>
-                                <select id="dname" class="form-control select2bs4" name="dname" style="width: 100%;" required>
-                                    <?php
-                                    // retrieving all Designation
-                                    $result = $conn->query("select * from designations");
-
-                                    while ($row = $result->fetch_assoc()) {
-                                        // displaying each Designation in the list
-                                        echo "<option value = '$row[designation_id]'>" . $row["designation"] . "</option>";
-                                    }
-                                    ?>
-                                </select>
-                            </div><br>
-
-                            <input type="hidden" id="canid" name="canid" value="<?php echo $row["candidate_id"]; ?>">
 
                             <button type="submit" class="btn btn-primary" style="float: right;">Submit</button>
 
