6b79b04bdc997667e4585f41a60b1027baa96756
SidWasTaken <siddharth.shyampur@gmail.com>
2022-02-28 15:04:42 +0530

    leave type update

---
+++ leavetype_update_query.php
@@ -0,0 +1,16 @@
+<?php
+// db connection file
+require_once 'db_conn.php';
+
+// fetches updated data from the form
+$leavetype = $_POST['leavetype_upd'];
+$upd_id = $_POST['lt_id'];
+
+// stores updated data into the database
+$res = $conn->query("UPDATE leave_types SET types='$leavetype' WHERE type_id='$upd_id'");
+
+// redirects to display employee information after closing connection
+$conn->close();
+header("location:emp_details.php?c=0");
+exit;
+?>
