a95960d8ffdf2b886dd028449e09be84a9348d1a
PrasadHendry <el.verethragna@gmail.com>
2022-03-01 13:13:21 +0530

    changed variable name in att-clock-out

--- attendance_clock_out.php
+++ attendance_clock_out.php
@@ -5,12 +5,12 @@ require_once 'db_conn.php';
 $attend_id = $_GET["aid"];
 
 // fetches current date and time
-$sal_date = new DateTime();
+$clockout_date = new DateTime();
 date_default_timezone_set('Asia/Calcutta');
-$sal_date = $sal_date->format('Y-m-d H:i:s');
+$clockout_date = $clockout_date->format('Y-m-d H:i:s');
 
 // stores updated data into the database
-$res = $conn->query("UPDATE attendance SET clock_out = '$sal_date' where attendance_id = '$attend_id'");
+$res = $conn->query("UPDATE attendance SET clock_out = '$clockout_date' where attendance_id = '$attend_id'");
 
 // redirects to display candidate information after closing connection
 $conn->close();
