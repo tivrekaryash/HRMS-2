8f2ffee8b816112bd0abd800634374abed071148
SidWasTaken <siddharth.shyampur@gmail.com>
2022-03-01 16:33:25 +0530

    overtime payment through clocking out

--- attendance_clock_out.php
+++ attendance_clock_out.php
@@ -16,14 +16,22 @@ $res = $conn->query("UPDATE attendance SET clock_out = '$clockout_date' where at
 $res = $conn->query("select * from attendance where attendance_id = '$attend_id'");
 $res = $res->fetch_assoc();
 
-// calculates time difference between clock in and clock out times
-$hours = 0;
+// calculates time difference between clock in and clock out times and rounds it off to the nearest lower integer
+$hours = floor(abs(strtotime($res["clock_out"]) - strtotime($res["clock_in"]))/3600);                                    // to change to upper limit use ceil instead of floor
 
-// calculates total amount of overtime pay accumulated on that day
-$amount = 0;
+// fetches employee data
+$res = $conn->query("select * from employee_information where employee_id = '$res[employee_id]'");
+$res = $res->fetch_assoc();
+
+// fetches amount per hour for the employee
+$desig_res = $conn->query("select * from designations where designation_id = '$res[designation_id]'");
+$desig_res = $desig_res->fetch_assoc();
+
+// calculates amount of overtime pay accumulated for the shift
+$amount = $hours * $desig_res["amt_per_hour"];
 
 // stores the difference as extra hours worked if any
-$res = $conn->query("insert into overtime_pay_emp (employee_id, hrs_worked, total_amt) values ('$res[employee_id]', '$hours', '$amount')");
+$res = $conn->query("insert into overtime_pay_emp (employee_id, otp_date, hrs_worked, total_amt) values ('$res[employee_id]', '$clockout_date', '$hours', '$amount')");
 
 // redirects to display candidate information after closing connection
 $conn->close();
