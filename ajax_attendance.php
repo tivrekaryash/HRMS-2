48c5f78e6bfec2cf7e9006e22bb49d607a3a06b9
SidWasTaken <siddharth.shyampur@gmail.com>
2022-03-01 13:09:49 +0530

    attendance records ajax

--- ajax_attendance.php
+++ ajax_attendance.php
@@ -2,14 +2,13 @@
 include 'db_conn.php'; 
 
 $userid = $_POST['userid'];
-$result = mysqli_query($conn, "select * from job_history where employee_id='$userid' order by history_id desc");
+$result = mysqli_query($conn, "select * from attendance where employee_id='$userid' order by attendance_id desc");
 
 $response = "<table style='text-align:center; background-color:white;' class='table table-bordered table-hover'><tr><th>Employee ID</th><th>Employee Name</th><th>Clock-in</th><th>Clock-out</th><th>Shift</th></tr>";
 
 while ($row = mysqli_fetch_array($result)) {
     $res = mysqli_query($conn, "select * from employee_information where employee_id = '$row[employee_id]'");
     $emprow = $res->fetch_assoc();
-
 
     $response .= "<tr><td>" . $row['employee_id'] . "</td>";
     $response .= "<td>" . $emprow['employee_name'] . "</td>";
