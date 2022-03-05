7e409ed6f637484ab60126874d83cc03a28739dc
SidWasTaken <siddharth.shyampur@gmail.com>
2022-03-05 12:45:39 +0530

    leave rejection

---
+++ leave_reject.php
@@ -0,0 +1,15 @@
+<?php
+// db connection file
+require_once 'db_conn.php';
+
+// fetches leave id to be rejected
+$del = $_GET['lacc'];
+
+// rejects the leave
+$res = $conn->query("UPDATE leaves SET approval='rejected' WHERE leave_id='$upd_id'");
+
+// redirects to display employee information after closing connection
+$conn->close();
+header("location:time_rec_view.php?c=3");
+exit;
+?>
