2d6e7810ec17506e5ae96c1f84eab3b46beb6c90
SidWasTaken <siddharth.shyampur@gmail.com>
2022-03-06 13:49:11 +0530

    employee salary update

---
+++ emp_per_sal_upd.php
@@ -0,0 +1,16 @@
+<?php
+// db connection file
+include 'db_conn.php';
+
+// fetches updated data from the form
+$sal_id = $_POST['psal_id'];
+$sal_amt = $_POST['emp_psal_upd'];
+
+// stores updated data into the database
+$res = $conn->query("UPDATE emp_personal_sal SET salary_amount_rec='$sal_amt' WHERE personal_sal_id='$sal_id'");
+
+// redirects to display employee information after closing connection
+$conn->close();
+header("location:emp_details.php?c=3");
+exit;
+?>
