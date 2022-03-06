4c44aba0e03da9ee8f9b92c99ca4624573649c57
SidWasTaken <siddharth.shyampur@gmail.com>
2022-03-06 12:54:59 +0530

    employee default salary

--- emp_desg_set.php
+++ emp_desg_set.php
@@ -13,6 +13,13 @@ $start_date = $start_date->format('Y-m-d');
 // stores updated data into the database
 $res = $conn->query("UPDATE employee_information SET designation_id='$desig_id' WHERE employee_id='$emp_id'");
 
+// fetches designation salary
+$res = $conn->query("select base_salary from designations WHERE designation_id='$desig_id'");
+$res = $res->fetch_assoc();
+
+// sets the employee's salary according to the designation
+$res = $conn->query("insert into emp_personal_sal (employee_id, salary_amount_rec) values ('$emp_id', '$res[base_salary]')");
+
 // updates job history record of employee
 $res = $conn->query("insert into job_history (employee_id, designation_id, job_start_date) values ('$emp_id', '$desig_id', '$start_date')");
 
