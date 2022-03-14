ac5a6422f9398570e631e25bc787494ab0c5c330
PrasadHendry <el.verethragna@gmail.com>
2022-03-14 15:35:22 +0530

    footer changed minor

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2021-2023 <a href="https://www.cosmicsolutions.co.in/">Cosmic Solutions</a>.</strong><br>
+    <strong>Copyright &copy; 2021-2023 <a href="https://www.cosmicsolutions.co.in/">Cosmic Solutions</a>.</strong>&nbsp;
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
         <b>Version</b> 1.0.0
