43a8ef349f789c52e49f26d3b974a93a3b3703bc
SidWasTaken <siddharth.shyampur@gmail.com>
2022-03-15 15:33:28 +0530

    session timeout fix

--- check_login.php
+++ check_login.php
@@ -1,12 +1,15 @@
 <?php
 session_start();
 if(isset($_SESSION['login']) & ($_SESSION['login'] == true)){
-	if(time()-$_SESSION["login_time"] >600)
+	if(time()-$_SESSION["login_time"] >10)
     {
         session_unset();
         session_destroy();
         header("location:login_form.php?lerr=3");  // 3: session has expired
     }
+
+    else
+    $_SESSION["login_time"] = time();
 }else{
 	// redirect user to login page
 	header("location:login_form.php");
