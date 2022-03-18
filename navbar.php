9ef3fcb0ad110baf2a03a7ea4c85eadc88221162
PrasadHendry <el.verethragna@gmail.com>
2022-03-18 10:46:16 +0530

    fullscreen remove

--- navbar.php
+++ navbar.php
@@ -12,12 +12,6 @@
 
       <!-- Right navbar links -->
       <ul class="navbar-nav ml-auto"> 
-
-          <li class="nav-item">
-              <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-                  <i class="fas fa-expand-arrows-alt"></i>
-              </a>
-          </li>
           <li class="nav-item">
               <a class="btn btn-outline-danger" href="logout.php" role="button">
               <i class="fa-solid fa-arrow-right-from-bracket"></i>
